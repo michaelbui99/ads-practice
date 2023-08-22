a2854f71ecfdfd92e4696f92747e8ae6c7ecd2c9
Michael Bui <michaelbui99@hotmail.com>
2023-08-22 19:30:52 +0200

    bfs adjacency matrix

--- ads_practice/graph.py
+++ ads_practice/graph.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import json
 from dataclasses import dataclass
+from ads_practice.queue import Queue
 
 
 @dataclass
@@ -36,3 +37,47 @@ class AdjacencyList:
 
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=False, indent=4)
+
+
+def bfs_adjmatrix(graph: list[list[int]], source: int, query: int) -> list[int]:
+    seen = [False] * len(graph)
+    prev = [-1] * len(graph)
+
+    q = Queue()
+    q.enqueue(source)
+    seen[source] = True
+
+    while not q.empty():
+        curr = q.dequeue()
+        if curr == query:
+            break
+
+        adjacent_vertices = graph[curr]
+        for i in range(0, len(adjacent_vertices)):
+            # No edge
+            if adjacent_vertices[i] == 0:
+                continue
+
+            # Already visited
+            if seen[i]:
+                continue
+
+            # Visit adjacent vertices
+            seen[i] = True
+            prev[i] = curr
+            q.enqueue(i)
+
+        seen[curr] = True
+
+    curr = query
+    res = []
+    while prev[curr] != -1:
+        res.append(curr)
+        curr = prev[curr]
+
+    if len(res) > 0:
+        res.reverse()
+        res = [source] + res
+        return res
+
+    return []
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,4 +1,4 @@
-from ads_practice.graph import AdjacencyList
+from ads_practice.graph import AdjacencyList, bfs_adjmatrix
 
 
 def test_adjlist_direction():
@@ -11,3 +11,18 @@ def test_adjlist_direction():
     assert graph.is_connected(1, 3) == True
     assert graph.is_connected(3, 1) == False
     assert graph.is_connected(2, 3) == False
+
+
+def test_bfs_adjmatrix():
+    graph = [
+        [0, 3, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 7, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 5, 0, 2, 0],
+        [0, 0, 18, 0, 0, 0, 1],
+        [0, 0, 0, 1, 0, 0, 1],
+    ]
+
+    assert bfs_adjmatrix(graph, 0, 6) == [0, 1, 4, 5, 6]
+    assert bfs_adjmatrix(graph, 6, 0) == []
